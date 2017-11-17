c671665e222b21907b034e0e1126ac834f024126
Mayurkumar Patel <mayurt20@gmail.com>
2017-11-17 11:55:06 +0530

    initial commit

---
+++ coure1/week1/MaxPairwiseProduct.rb
@@ -0,0 +1,48 @@
+require 'benchmark'
+
+class MaxPairwiseProduct
+	attr_accessor :array
+
+	def initialize(array)
+		@array = array
+	end
+
+	def max_number1
+		max1 = 0
+		index = 0
+		array.each do |a|
+			if a > max1
+				max1 = a
+				index = array.index(a)
+			end
+		end
+		array[index] = -1
+		max1
+	end
+
+	def max_number2
+		max2 = 0
+		array.each do |a|
+			if a > max2
+				max2 = a
+			end
+		end
+		max2
+	end
+
+	def max_pair_wise_product
+		max1 = max_number1
+		max2 = max_number2
+		max1 * max2
+	end
+end
+
+N = 1000000
+array = []
+N.times {|n| array.push(Random.new.rand(10))}
+
+Benchmark.bm do |x|
+	x.report("MaxPairwiseProduct:") do 
+		MaxPairwiseProduct.new(array).max_pair_wise_product
+	end
+end
